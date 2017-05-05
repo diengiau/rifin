2dc9e50139e22bc09587c8712c3bc85046df8150
buidiengiau <buidiengiau@gmail.com>
2017-05-06 01:27:29 +0800

    add data `key`

---
+++ R/key.R
@@ -0,0 +1,7 @@
+#' key data
+#' ver: 0.1.0 in May 2017
+#' author: Dien Giau Bui (aka Richard)
+#'
+#' This is the datasets for `ri_fmatch` function which includes 2 data: name_old and key.
+#'
+"key"
