408e2aba089e5367b8756000a9f6bb4d1cf82624
buidiengiau <buidiengiau@gmail.com>
2017-05-08 23:12:45 +0800

    change name key.R to ri_key.R

--- R/ri_key.R
+++ R/ri_key.R
@@ -1,7 +1,7 @@
-#' key data
+#' ri_key data
 #' ver: 0.1.0 in May 2017
 #' author: Dien Giau Bui (aka Richard)
 #'
 #' This is the datasets for `ri_fmatch` function which includes 2 data: name_old and key.
 #'
-"key"
+"ri_key"
